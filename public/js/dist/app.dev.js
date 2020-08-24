6798e44de63463b03d7ac1a47f1ebc3d061edb0d
Jorge Salas <j.saulff@gmail.com>
2020-08-24 16:29:50 -0500

    Vegas implementation

---
+++ public/js/dist/app.dev.js
@@ -0,0 +1,40 @@
+"use strict";
+
+$(document).ready(function () {
+  setTimeout(function () {
+    $('main').addClass('fadeIn');
+    $('main').css('opacity', '1');
+    $('#loader-wrapper').addClass('fadeOut');
+    $('#loader-wrapper').css('opacity', '0');
+  }, 3000);
+  $(function () {
+    $('main').vegas({
+      slides: [{
+        src: './img/slide-1.jfif'
+      }, {
+        src: './img/slide-2.jpg'
+      }],
+      timer: false,
+      transition: ['zoomOut']
+    });
+  });
+  var swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect: 'fade',
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
+});
